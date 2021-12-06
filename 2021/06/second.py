64bac2568eea5c1b0bcafe9835011fa6934d6c1d
Magnus Jacobsen <magnusjacobsen@gmx.com>
2021-12-06 17:53:49 +0100

    did stuff on python solution

--- 2021/06/second.py
+++ 2021/06/second.py
@@ -1,5 +1,4 @@
-from functools import reduce
-vals = list(map(int, open('input').readlines()[0].split(',')))
+vals = [int(x) for x in open('input').readline().split(',')]
 counts = [0]*9
 for i in vals:
     counts[i] += 1
@@ -8,8 +7,3 @@ for _ in range(256):
     counts.append(tmp)
     counts[6] += tmp
 print(sum(counts))
-
-
-
-#print(reduce(lambda x, acc: list(map(lambda i: acc[i] + 1 if i == x else acc[i], range(9))), list(map(int, open('input').readlines()[0].split(','))), [0]*9))
-
--- 2021/06/second.rs
+++ 2021/06/second.rs
@@ -1,4 +1,4 @@
 #![feature(stdin_forwarders)]
 fn main() {
-    println!("{}", (0..256).fold(std::io::stdin().lines().next().unwrap().unwrap().split(',').map(|x| x.parse::<usize>().unwrap() ).fold(vec![0;9], |acc, x| (0..9).map(|i| if i == x { acc[i] + 1 } else { acc[i] }).collect()), |acc, _| (0..9).map(|i| if i == 6 { acc[i + 1] + acc[0] } else if i == 8 { acc[0] }  else { acc[i + 1] }).collect() ).iter().sum::<u128>());
+    println!("{}", (0..256).fold(std::io::stdin().lines().next().unwrap().unwrap().split(',').map(|x| x.parse::<usize>().unwrap() ).fold(vec![0;9], |initial_fish, x| (0..9).map(|i| if i == x { initial_fish[i] + 1 } else { initial_fish[i] }).collect()), |fish, _| (0..9).map(|i| if i == 6 { fish[i + 1] + fish[0] } else if i == 8 { fish[0] }  else { fish[i + 1] }).collect()).iter().sum::<u128>());
 }
