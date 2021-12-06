c796d8b512fe3af63e01d53be65adb9ba27a70ee
Magnus Jacobsen <magnusjacobsen@gmx.com>
2021-12-06 17:55:05 +0100

    python be idk

--- 2021/06/second.py
+++ 2021/06/second.py
@@ -1,9 +1,10 @@
-vals = [int(x) for x in open('input').readline().split(',')]
 counts = [0]*9
-for i in vals:
+for i in  [int(x) for x in open('input').readline().split(',')]:
     counts[i] += 1
+
 for _ in range(256):
     tmp = counts.pop(0)
     counts.append(tmp)
     counts[6] += tmp
+
 print(sum(counts))
