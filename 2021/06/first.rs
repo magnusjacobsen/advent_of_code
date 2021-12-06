a5ced40bb57653105ace167e08fb00b2e3907918
Magnus Jacobsen <magnusjacobsen@gmx.com>
2021-12-06 17:47:01 +0100

    removed explicit imports from 06

--- 2021/06/first.rs
+++ 2021/06/first.rs
@@ -1,6 +1,4 @@
-use std::io::{self, BufRead};
-use std::str::FromStr;
-
+#![feature(stdin_forwarders)]
 fn main() {
-    println!("{}", (0..80).fold(io::stdin().lock().lines().next().unwrap().unwrap().split(',').map(|x| usize::from_str(x).unwrap() ).fold(vec![0;9], |acc, x| (0..9).map(|i| if i == x { acc[i] + 1 } else { acc[i] }).collect()), |acc, _| (0..9).map(|i| if i == 6 { acc[i + 1] + acc[0] } else if i == 8 { acc[0] }  else { acc[i + 1] }).collect() ).iter().sum::<u128>());
+    println!("{}", (0..80).fold(std::io::stdin().lines().next().unwrap().unwrap().split(',').map(|x| x.parse::<usize>().unwrap() ).fold(vec![0;9], |acc, x| (0..9).map(|i| if i == x { acc[i] + 1 } else { acc[i] }).collect()), |acc, _| (0..9).map(|i| if i == 6 { acc[i + 1] + acc[0] } else if i == 8 { acc[0] }  else { acc[i + 1] }).collect() ).iter().sum::<u128>());
 }
