ff586ef54faf99b7146648307511a191ee32585c
Magnus Malthe Jacobsen <magnus.jacobsen@pdc.com>
2022-12-01 17:35:10 +0100

    made first.rs oneline

--- 2022/01/first.rs
+++ 2022/01/first.rs
@@ -1,10 +1,5 @@
 use std::io::{self, BufRead};
-use std::cmp::max;
 
 fn main() {
-    let(a, b) = io::stdin().lock().lines().fold((0, 0), |acc, current| match current.unwrap().as_str() {
-        "" => (max(acc.0, acc.1), 0),
-        num => (acc.0, u32::from_str_radix(&num, 10).unwrap() + acc.1),
-    });
-    println!("{}", max(a, b));
+    println!("{}", io::stdin().lock().lines().fold([0, 0], |acc, current| match current.unwrap().as_str() {"" => [*acc.iter().max().unwrap(), 0], num => [acc[0], u32::from_str_radix(&num, 10).unwrap() + acc[1]], }).iter().max().unwrap());
 }
