1ce0e66caa42d041ff557f5b86312a5cd77d9571
magu0 <magnusjacobsen@gmx.com>
2024-12-16 23:25:09 +0100

    day 7

---
+++ 2024/07/second.rs
@@ -0,0 +1,31 @@
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
+fn solve(inc: i64, result: i64, rest: &[i64]) -> i64 {
+    if inc > result {
+        return inc;
+    }
+    if rest.len() == 0 {
+        return inc;
+    }
+    if result == solve(inc * rest[0], result, &rest[1..]) {
+        return result;
+    } else if result == solve(inc + rest[0], result, &rest[1..]) {
+        return result
+    }
+    return solve(inc * 10_i64.pow(rest[0].checked_ilog10().unwrap_or(0) + 1) + rest[0], result, &rest[1..]);
+}
+
+fn main() {
+    let lines = io::stdin().lock().lines().map(Result::unwrap).collect::<Vec<_>>();
+    let mut total_result = 0;
+    for line in lines {
+        let splitted = line.split(": ").collect::<Vec<_>>();
+        let result = i64::from_str(splitted[0]).unwrap();
+        let numbers = splitted[1].split(" ").map(i64::from_str).map(Result::unwrap).collect::<Vec<i64>>();
+        if result == solve(numbers[0], result, &numbers[1..]) {
+            total_result += result;
+        }
+    }
+    println!("{}", total_result);
+}
